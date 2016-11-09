9b2f9b8de7afb479e82847e5f07dc7504b8c74ab
mrshoor <mrshoor@yandex.ru>
2016-11-08 21:22:25 -0800

    missed files

---
+++ Demos/Src/QuantumWorldGenerator/QWG.dpr
@@ -0,0 +1,14 @@
+program QWG;
+
+uses
+  Vcl.Forms,
+  untmain in 'untmain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
